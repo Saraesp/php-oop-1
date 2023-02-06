eedbc81b5622d16bcf6e1a9b8dabb00ee3b6f77c
Saraesp <saraesposito145@gmail.com>
2023-02-06 21:12:06 +0100

    correzione esercizio

--- index.php
+++ index.php
@@ -4,17 +4,21 @@
         public $title;
         public $director;
         public $production_date;
-        public $language;
         public $vote;
         public $stars;
-        public $genere;
+        public $generi;
 
+        public function __construct($title, $director, $production_date, $generi){
 
-        function __construct($_title, $_director, $_production_date, $_language){
-            $this->title = $_title;
-            $this->director = $_director;
-            $this->production_date = $_production_date;
-            $this->language = $_language;
+            $this->title = $title;
+            $this->director = $director;
+            $this->production_date = $production_date;
+            $this->generi = $generi;
+
+        }
+
+        public function getMovies(){
+            return $this->production_date." - ".$this->director." - ".$this->generi_string_print();
         }
 
         public function setVoteFilm($stars) {
@@ -27,41 +31,41 @@
             return $this->vote;
         }
 
+        public function setTitle(){
+            $this->title =$_title;
+        }
+
+        public function getTitle(){
+            return $this->title;
+        }
+
+        public function generi_string_print(){
+             
+            $generi = $this->generi;
+            $generi_string = '';
+
+            foreach($generi as $genere){
+                $generi_string .= $genere->name." ";
+            }
+
+            return $generi_string;
+        }
+
     }
 
     class Genre {
-        public $animazione;
-        public $biografia;
-        public $comico;
-        public $avventura;
 
-        function __construct($_animazione, $_biografia, $_comico, $_avventura){
-            $this->animazione = $_animazione;
-            $this->biografia = $_biografia;
-            $this->comico = $_comico;
-            $this->avventura = $_avventura;
+        public $name;
+
+        function __construct($_name){
+            $this->name = $_name;
         }
     }
+  
 
-    //CREAZIONE OGGETTO
-    $movie_1 = new Movie('Inception', 'Christopher Nolan', '2010-11-18', 'English');
-    // $movie_2 = new Movie('Harry Potter e la pietra filosofale', 'David Heyman');
-
-    $genere_1 = new Genre ('Animazione', 'Biografia','Comico', 'Avventura' );
-    $genere_2 = new Genre ('Drammatico', 'Storico','Fantascienza', 'Azione' );
-
-    $movie_1->genere = [$genere_1, $genere_2];
-
-    echo "<pre>";
-    var_dump($movie_1);
-    echo "</br>";
-
-    //ASSEGNAZIONE VALORI
-    $movie_1->title = 'Inception';
-    $movie_1->director = 'Christopher Nolan';
-    $movie_1->production_date = '2010-11-18';
-    $movie_1->language = 'English';
-
+    // echo "<pre>";
+    // var_dump($movie_1);
+    // echo "</br>";
 
     // $movie_2->title = 'Harry Potter e la pietra filosofale';
     // $movie_2->director = 'David Heyman';
@@ -72,34 +76,58 @@
 
 ?>
 
+<?php 
+
+        $azione = new Genre('Azione');
+        $fantascienza = new Genre('Fantascienza');
+
+        $generi = [
+                $azione,
+                $fantascienza
+            ];
+
+        $inception = new Movie('Inception', 'Christopher Nolan', '2010-11-18', $generi);
+
+        $movies = [
+            $inception,
+        ];
+
+        // $movie_1->stars = 3;
+        // $movie_2->stars = 4.5;
+
+        // $movie_1->setVoteFilm($movie_1->stars);
+        // echo "<br>".$movie_1->getVoteFilm();
+
+        // $movie_2->setVoteFilm($movie_2->stars);
+        // echo "<br>".$movie_2->getVoteFilm();
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Movie</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <pre>
-                    <?php 
-                        // $movie_1->stars = 3;
-                        // $movie_2->stars = 4.5;
-
-                        // $movie_1->setVoteFilm($movie_1->stars);
-                        // echo "<br>".$movie_1->getVoteFilm();
-
-                        // $movie_2->setVoteFilm($movie_2->stars);
-                        // echo "<br>".$movie_2->getVoteFilm();
-                    ?>
-                </pre>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php foreach($movies as $movie) { ?>
+                        <div class="col-12 col-md-4">
+                            <div class="card">
+                                <img class="card-img-top" src="./image/inception_image.jpg" alt="">
+                                <div class="card-body">
+                                    <h4><?php echo $movie->getTitle(); ?></h4>
+                                    <p><?php echo $movie->getMovies(); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
